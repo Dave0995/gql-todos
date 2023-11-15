56c488136636f951247c75d14d6b9ccfac5f8f73
DavidJS01 <davidshipmanchs2020@gmail.com>
2023-11-14 20:49:00 -0700

    yay

---
+++ hola.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "!Hola desde Utah!\n";
+    std::cout << "Y ahora, desde Provo!\n";
+    std::cout << "Y ahora, desde Orem!\n";
+    std::cout << "Y ahora, volvemos a Provo!\n";
+
+    std::cout << "Estoy aprendiendo C++, pero es dificil ): \n";
+    std::cout << "Dave es el mejor programador del Atlas!\n";
+    std::cout << "Adios Dave!";
+    return 0;
+}
